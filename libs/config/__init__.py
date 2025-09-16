ce1b02daa193ee8589af396f2d59356524c6c92a
Naitri Doshi <jesaldoshi123@gmail.com>
2025-09-16 18:06:21 +0530

    Fix Config File Issues for Deployment

--- libs/config/__init__.py
+++ libs/config/__init__.py
@@ -3,32 +3,25 @@ from os import path
 
 from dotenv import dotenv_values
 
-env_path = ".env"
-
-if not path.exists(env_path):
-    raise Exception(".env request_file not found")
-
-config = dotenv_values(env_path)
-
 CURRENT_WORKING_DIR = os.getcwd()
 
-PINECONE_API_KEY = config.get("PINECONE_API_KEY")
-PINECONE_INDEX_NAME = config.get("PINECONE_INDEX_NAME")
-OPENAI_API_KEY = config.get("OPENAI_API_KEY")
-CHROMA_COLLECTION_NAME = config.get("CHROMA_COLLECTION_NAME")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHROMA_COLLECTION_NAME = os.getenv("CHROMA_COLLECTION_NAME")
 CHROMA_PERSIST_DIRECTORY = os.path.join(
-    CURRENT_WORKING_DIR, config.get("CHROMA_PERSIST_DIRECTORY")
+    CURRENT_WORKING_DIR, os.getenv("CHROMA_PERSIST_DIRECTORY")
 )
-OPENAI_EMBEDDING_MODEL = config.get("OPENAI_EMBEDDING_MODEL")
-OPENAI_MODEL = config.get("OPENAI_MODEL")
-GEMINI_MODEL = config.get("GEMINI_MODEL")
-GEMINI_API_KEY = config.get("GEMINI_API_KEY")
-MONGO_URI = config.get("MONGO_URI")
-DATABASE_NAME = config.get("DATABASE_NAME")
-HOST = config.get("HOST")
-PORT = int(config.get("PORT"))
-ENVIRONMENT = config.get("ENVIRONMENT")
-ANTHROPIC_MODEL = config.get("ANTHROPIC_MODEL")
-ANTHROPIC_API_KEY = config.get("ANTHROPIC_API_KEY")
+OPENAI_EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL")
+GEMINI_MODEL = os.getenv("GEMINI_MODEL")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+ANTHROPIC_MODEL = os.getenv("ANTHROPIC_MODEL")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
-GOOGLE_EMBEDDING_MODEL =config.get("GOOGLE_EMBEDDING_MODEL")
+GOOGLE_EMBEDDING_MODEL =os.getenv("GOOGLE_EMBEDDING_MODEL")
